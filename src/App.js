3326999760a4fc519b6ba1393374d152e16d670f
Zied <zied-rj@hotmail.com>
2022-11-21 09:43:50 +0100

    first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import FormsComponent from "./components/FormsComponent";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <FormsComponent />
+      </header>
+    </div>
+  );
+}
+
+export default App;
